b716e3b26452029f7ee43d571eaeb2f3ffc6a4cf
Nashwan Abdullah <35005761+codenashwan@users.noreply.github.com>
2021-09-09 16:59:06 +0300

    Update config.php

--- api/includes/config.php
+++ api/includes/config.php
@@ -1,6 +1,5 @@
 <?php
 $db = mysqli_connect("localhost"  , "root" , "" , "radio");
 
-// $mypassword = 'db4d9992897eda89b50f1d3208db607902da7e79c6f3bc6e6933cc5919068564'; // default = "password"
-$mypassword = '11e10589bd9c44e628a4360d9da65113dcb338118054fece95fa3ca1e0b0b8ee'; 
+$mypassword = 'db4d9992897eda89b50f1d3208db607902da7e79c6f3bc6e6933cc5919068564'; // default = "password"
 ?>
